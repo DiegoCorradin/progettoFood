58d8372e932a6ed7a4f8e12431652dbefe86c327
utente <utente@MOB2-34.Marconi.loc>
2021-05-17 23:57:52 +0200

    wtfforms e routing

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
 Bootstrap(app)
 
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=30)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='invalid email') ,Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=30)])
 
 @app.route("/")
 def index():
@@ -15,15 +30,20 @@ def index():
 def child():
     return render_template("child.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template("login.html", form=form)
 
 @app.route("/signup")
 def signup():
-    return render_template("signup.html")
+    form = RegisterForm()
+    return render_template("signup.html", form=form)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
